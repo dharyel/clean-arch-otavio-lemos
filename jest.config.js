bcca68514f6290a2aefe9a107c37aaa930c2a0ba
Dharyel <dshfoxdev@gmail.com>
2022-09-02 07:47:00 -0300

    chore: add jest

---
+++ jest.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  preset: '@shelf/jest-mongodb',
+  roots: ['<rootDir>/src'],
+  clearMocks: true,
+  coverageDirectory: 'coverage',
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!**/test/**',
+    '!**/config/**'
+  ],
+  testEnvironment: 'node',
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  },
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+    '^@test/(.*)$': '<rootDir>/test/$1'
+  },
+  setupFiles: ['dotenv/config']
+}
