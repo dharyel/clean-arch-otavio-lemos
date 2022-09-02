6439ee322e2473126e7c441fe2614f5210fb72e2
Dharyel <dshfoxdev@gmail.com>
2022-09-02 14:26:48 -0300

    feat: add UserData interface

--- jest.config.js
+++ jest.config.js
@@ -1,19 +1,19 @@
 module.exports = {
-  preset: '@shelf/jest-mongodb',
-  roots: ['<rootDir>/src'],
-  clearMocks: true,
-  coverageDirectory: 'coverage',
-  collectCoverageFrom: [
-    '<rootDir>/src/**/*.ts',
-    '!**/test/**',
-    '!**/config/**'
-  ],
-  testEnvironment: 'node',
-  transform: {
-    '.+\\.ts$': 'ts-jest'
-  },
-  moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/src/$1',
-    '^@test/(.*)$': '<rootDir>/test/$1'
-  }
+    preset: '@shelf/jest-mongodb',
+    roots: ['<rootDir>/src'],
+    clearMocks: true,
+    coverageDirectory: 'coverage',
+    collectCoverageFrom: [
+        '<rootDir>/src/**/*.ts',
+        '!**/test/**',
+        '!**/config/**'
+    ],
+    testEnvironment: 'node',
+    transform: {
+        '.+\\.ts$': 'ts-jest'
+    },
+    moduleNameMapper: {
+        '^@/(.*)$': '<rootDir>/src/$1',
+        '^@test/(.*)$': '<rootDir>/test/$1'
+    }
 }
