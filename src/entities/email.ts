4d08968ebda265cc71ddac52b93d6195de5f80f5
Dharyel <dshfoxdev@gmail.com>
2022-09-07 21:26:07 -0300

    feat: validate local part larger than 64 chars

--- src/entities/email.ts
+++ src/entities/email.ts
@@ -2,8 +2,12 @@ export class Email {
     static validate (email: string) {
         if (!email) {
             return false
-        } else {
-            return true
         }
+        const [local] = email.split('@')
+        if (local.length > 64) {
+            return false
+        }
+
+        return true
     }
 }
