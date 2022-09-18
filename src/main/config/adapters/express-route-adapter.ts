b8fd02fabf39a925b8114e6b2456ae0d6b52fccb
Dharyel <dshfoxdev@gmail.com>
2022-09-18 09:06:05 -0300

    feat: express route adapter

---
+++ src/main/config/adapters/express-route-adapter.ts
@@ -0,0 +1,14 @@
+import { RegisterUserController, HttpRequest } from '@/web-controllers/'
+import { Request, Response } from 'express'
+
+export const adaptRoute = (controller: RegisterUserController) => {
+    return async (req: Request, res: Response) => {
+        const httpRequest: HttpRequest = {
+            body: req.body
+        }
+
+        const httpResponse = await controller.handle(httpRequest)
+
+        res.status(httpResponse.statusCode).json(httpResponse.body)
+    }
+}
