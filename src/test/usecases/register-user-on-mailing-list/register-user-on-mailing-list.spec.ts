7510bc5c5e1e17ecd8657b450f806ee975fdd798
Dharyel <dshfoxdev@gmail.com>
2022-09-21 16:51:43 -0300

    test: verify invalid data

--- src/test/usecases/register-user-on-mailing-list/register-user-on-mailing-list.spec.ts
+++ src/test/usecases/register-user-on-mailing-list/register-user-on-mailing-list.spec.ts
@@ -17,32 +17,4 @@ describe('Register user on mailing list use case', () => {
         expect(user.name).toBe('any_name')
         expect(response.value.name).toBe('any_name')
     })
-
-    it('should not add user with invalid e-mail to mailing list', async () => {
-        const users: UserData[] = []
-
-        const repo: UserRepository = new InMemoryUserRepository(users)
-        const usecase: RegisterUserOnMailingList = new RegisterUserOnMailingList(repo)
-        const name = 'any_name'
-        const invalidEmail = 'invalid.mail'
-        const response = (await usecase.perform({ name, email: invalidEmail })).value as Error
-        const user = await repo.findUserByEmail('invalid.mail')
-
-        expect(user).toBe(null)
-        expect(response.name).toEqual('InvalidEmailError')
-    })
-
-    it('should not add user with invalid name to mailing list ', async () => {
-        const users: UserData[] = []
-
-        const repo: UserRepository = new InMemoryUserRepository(users)
-        const usecase: RegisterUserOnMailingList = new RegisterUserOnMailingList(repo)
-        const invalidName = ''
-        const email = 'any@email.com'
-        const response = (await usecase.perform({ name: invalidName, email })).value as Error
-        const user = await repo.findUserByEmail('any@email.com')
-
-        expect(user).toBe(null)
-        expect(response.name).toEqual('InvalidNameError')
-    })
 })
