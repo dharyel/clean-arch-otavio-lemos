f2104bceb9e3e83515ec8f3df15d860219d00515
Dharyel <dshfoxdev@gmail.com>
2022-09-21 14:25:00 -0300

    feat: handle invalid user

--- src/usecases/send-email/send-email.ts
+++ src/usecases/send-email/send-email.ts
@@ -1,5 +1,6 @@
-import { UserData } from '@/entities'
-import { Either } from '@/shared'
+import { User, UserData } from '@/entities'
+import { InvalidEmailError, InvalidNameError } from '@/entities/errors'
+import { Either, left } from '@/shared'
 import { UseCase } from '@/usecases/ports/use-case'
 import { MailServiceError } from '../errors/mail-service-error'
 import { EmailOptions, EmailService } from './ports/email-service'
@@ -7,8 +8,16 @@ import { EmailOptions, EmailService } from './ports/email-service'
 export class SendEmail implements UseCase {
     constructor (private readonly emailOptions: EmailOptions, private readonly emailService: EmailService) {}
 
-    async perform (userData: UserData): Promise<Either<MailServiceError, EmailOptions>> {
-        const greetings = `E aí, <b>${userData.name}</b>. Beleza?`
+    async perform (userData: UserData): Promise<Either<InvalidNameError | InvalidEmailError | MailServiceError, EmailOptions>> {
+        const userOrError: Either<InvalidNameError | InvalidEmailError, User> = User.create(userData)
+
+        if (userOrError.isLeft()) {
+            return left(userOrError.value)
+        }
+
+        const user = userOrError.value
+
+        const greetings = `E aí, <b>${user.name}</b>. Beleza?`
         const customizedHtml = `${greetings}<br><br>${this.emailOptions.html}`
         const { host, port, username, password, from, subject, text, attachments } = this.emailOptions
 
@@ -18,7 +27,7 @@ export class SendEmail implements UseCase {
             username,
             password,
             from,
-            to: userData.name + '<' + userData.email + '>',
+            to: user.name + '<' + user.email + '>',
             subject,
             text,
             html: customizedHtml,
