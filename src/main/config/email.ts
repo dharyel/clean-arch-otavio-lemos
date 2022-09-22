dec02c99f93ad626cfc46eea1e812b230e0fcfe7
Dharyel <dshfoxdev@gmail.com>
2022-09-22 09:17:21 -0300

    feat: use register and send email usecase

---
+++ src/main/config/email.ts
@@ -0,0 +1,25 @@
+import { EmailOptions } from '@/usecases/send-email/ports/email-service'
+
+const attachments = [{
+    filename: 'text.txt',
+    path: '../../resources/text.txt'
+}]
+
+export function getEmailOptions (): EmailOptions {
+    const from = 'Dharyel | TheWiseDev<dshfoxdev@gmail.com>'
+    const to = ''
+    const mailOptions: EmailOptions = {
+        host: process.env.EMAIL_HOST as string,
+        port: Number.parseInt(process.env.EMAIL_PORT),
+        username: process.env.EMAIL_USERNAME,
+        password: process.env.EMAIL_PASSWORD,
+        from,
+        to,
+        subject: 'Mensagem de teste',
+        text: 'Texto da mensagem',
+        html: '<b>Texto da mensagem</b>',
+        attachments
+    }
+
+    return mailOptions
+}
--- src/main/factories/register.ts
+++ src/main/factories/register.ts
@@ -2,13 +2,20 @@ import { RegisterAndSendEmailController } from '@/web-controllers'
 import { RegisterUserOnMailingList } from '@/usecases'
 // import { InMemoryUserRepository } from '@/usecases/register-user-on-mailing-list/repository'
 import { MongodbUserRepository } from '@/external/repositories/mongodb'
+import { SendEmail } from '@/usecases/send-email/send-email'
+import { NodemailerEmailService } from '@/external/mail-services'
+import { getEmailOptions } from '@/main/config/email'
+import { RegisterAndSendEmail } from '@/usecases/register-and-send-email/register-and-send-email'
 
-export const makeRegisterUserController = (): RegisterAndSendEmailController => {
+export const makeRegisterAndSendEmailController = (): RegisterAndSendEmailController => {
     // const inMemoryUserRepository = new InMemoryUserRepository([])
     // const registerUserOnMailingListUseCase = new RegisterUserOnMailingList(inMemoryUserRepository)
     const mongodbUserRepository = new MongodbUserRepository()
     const registerUserOnMailingListUseCase = new RegisterUserOnMailingList(mongodbUserRepository)
-    const registerUserController = new RegisterAndSendEmailController(registerUserOnMailingListUseCase)
+    const emailService = new NodemailerEmailService()
+    const sendEmailUseCase = new SendEmail(getEmailOptions(), emailService)
+    const registerAndSendEmailUseCase = new RegisterAndSendEmail(registerUserOnMailingListUseCase, sendEmailUseCase)
+    const registerUserController = new RegisterAndSendEmailController(registerAndSendEmailUseCase)
 
     return registerUserController
 }
